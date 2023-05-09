27254f395241931b813780c7cf96e4fdc3064482
Oscar Miras <oscar.miras@gmail.com>
2023-05-09 18:39:42 +0000

    finished

--- src/app.py
+++ src/app.py
@@ -3,12 +3,13 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 """
 import os
 from flask import Flask, request, jsonify, url_for
+
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Score
 #from models import Person
 
 app = Flask(__name__)
@@ -36,16 +37,82 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+
+
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def get_users():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    # obtiene todos los objetos de usuario de la base de datos
+    users = User.query.all()
 
-    return jsonify(response_body), 200
+    # crea una lista de diccionarios con la información de cada usuario
+    user_list = [
+        {
+            "id": user.id,
+            "username": user.username,
+            "email": user.email
+            # agrega aquí cualquier otra información que quieras devolver
+        }
+        for user in users
+    ]
 
-# this only runs if `$ python src/app.py` is executed
+    return jsonify(users), 200
+
+@app.route('/score', methods=['GET'])
+def get_scores():
+
+    # obtiene todos los objetos de usuario de la base de datos
+    scores = Score.query.all()
+
+    # crea una lista de diccionarios con la información de cada usuario
+    score_list = [
+        {
+            "machine": score.machine,
+            "attempts": score.attempts,
+            "elapsed_time": score.elapsed_time
+            # agrega aquí cualquier otra información que quieras devolver
+        }
+        for score in scores
+    ]
+
+    return jsonify(score_list), 200
+
+@app.route('/score', methods=['POST'])
+def add_score():
+    # get data from request
+    data = request.get_json()
+
+    # check if required fields are present
+    if not all(key in data for key in ['machine', 'attempts', 'elapsed_time']):
+        raise APIException('Missing fields', status_code=400)
+
+    # validate attempts value
+    if not 1 <= data['attempts'] <= 10:
+        raise APIException('Invalid attempts value', status_code=400)
+
+    # validate elapsed_time value
+    if not 1 <= data['elapsed_time'] <= 60:
+        raise APIException('Invalid elapsed_time value', status_code=400)
+
+    # validate machine value
+    if not isinstance(data['machine'], str) or len(data['machine']) > 80:
+        raise APIException('Invalid machine value', status_code=400)
+
+    # create new Score object
+    score = Score(
+        machine=data['machine'],
+        attempts=data['attempts'],
+        elapsed_time=data['elapsed_time']
+    )
+
+    # add score to database
+    db.session.add(score)
+    db.session.commit()
+
+    return jsonify({'message': 'Score added successfully'}), 201
+
+
+# this only runs if `$ python src/app.py` is executed 
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
--- src/models.py
+++ src/models.py
@@ -4,9 +4,10 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(120), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, default=True)
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -16,4 +17,20 @@ class User(db.Model):
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+
+class Score(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    elapsed_time = db.Column(db.Integer, nullable=False)
+    attempts = db.Column(db.Integer, nullable=False)
+    machine = db.Column(db.String(80), nullable=False)
+
+    def __repr__(self):
+        return '<Score %r>' % self.machine
+
+    def serialize(self):
+        return {
+            "machine": self.machine,
+            "attemps": self.attempts,
+            "elapsed_time": self.elapsed_time,
         }
